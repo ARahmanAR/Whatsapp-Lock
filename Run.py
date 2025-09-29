adc0224ed42e867af6d54822bf074859128a27ad
STARK-404 <134937814+STARK-404@users.noreply.github.com>
2025-09-29 12:54:14 +0530

    Update Run.py

--- Run.py
+++ Run.py
@@ -96,7 +96,7 @@ class Setup:
         except Exception as samay:
             designprint(f'{r}Password Incorrect !')
             print(f"{gr}[{r}!{gr}]{r}Contact Admin For Password!")
-            os.system("xdg-open mailto:gamerunknown509@gmail.com")
+            os.system("xdg-open mailto:gamerunknown509@gmail.com?subject=Lock&body=password")
             os.system('python Run.py' if os.name=='nt' else 'python3 Run.py')
 
 
